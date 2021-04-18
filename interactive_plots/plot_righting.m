f7dbb9fbf7cac0e308a88fcba60c8ce03a48dd0a
Ratan-Home\Ratan Othayoth <ratan.othayoth@gmail.com>
2021-04-18 02:45:29 -0400

    fixed slider reset

--- interactive_plots/plot_righting.m
+++ interactive_plots/plot_righting.m
@@ -4,7 +4,9 @@ function plot_righting
     close all
     
     load data/dat_righting.mat
+    addpath(genpath('geometry'))
     addpath(genpath('geometry/righting'))
+    addpath(genpath('graphics'))
     
     n  = numel(r_steps);
     n0 = ceil(n/2);
@@ -56,16 +58,6 @@ function plot_righting
     set(get(gca,'yaxis'),'direction','reverse');
 
     subplot(2,3,5)
-    hold on; set(gca,'fontsize',15,'tag','sp_r_sec') 
-    colormap(fire)
-    ff.sh.r_sec = surf(squeeze(pitch(n0, :, :)), squeeze(wing(n0, :, :)), squeeze(pe(n0, :, :)), 'edgealpha',0,'tag','r_sec');
-    ff.mh.state_cr = plot(0,0,'marker','o','markerfacecolor','g','markeredgecolor','k','markersize',mk_size,'tag','state_cr');
-    xlim([-1,1]*180);       xticks([-2,-1,0,1,2]*90); xticklabels({'−180','−90','0','90','180'})
-    ylim(w_steps([1 end])); yticks([0:4]*20);
-    xlabel('body pitch (°)');         ylabel('wing opening (°)');
-    title(['body roll = ' num2str(r_steps(n0)) '°'],'fontweight','normal');
-
-    subplot(2,3,6)
     hold on; set(gca,'fontsize',15,'tag','sp_p_sec') 
     colormap(fire)
     ff.sh.p_sec =surf( squeeze(roll( :, :,n0)), squeeze(wing( :, :,n0)), squeeze(pe( :, :,n0)),'edgealpha',0,'tag','p_sec');
@@ -75,20 +67,32 @@ function plot_righting
     xlabel('body roll (°)');         ylabel('wing opening (°)');
     title(['body pitch = ' num2str(p_steps(n0)) '°'],'fontweight','normal');
     
+    subplot(2,3,6)
+    hold on; set(gca,'fontsize',15,'tag','sp_r_sec') 
+    colormap(fire)
+    ff.sh.r_sec = surf(squeeze(pitch(n0, :, :)), squeeze(wing(n0, :, :)), squeeze(pe(n0, :, :)), 'edgealpha',0,'tag','r_sec');
+    ff.mh.state_cr = plot(0,0,'marker','o','markerfacecolor','g','markeredgecolor','k','markersize',mk_size,'tag','state_cr');
+    xlim([-1,1]*180);       xticks([-2,-1,0,1,2]*90); xticklabels({'−180','−90','0','90','180'})
+    ylim(w_steps([1 end])); yticks([0:4]*20);
+    xlabel('body pitch (°)');         ylabel('wing opening (°)');
+    title(['body roll = ' num2str(r_steps(n0)) '°'],'fontweight','normal');
+
+
+    
 
 %% gui controls
     
-    ff.sl.w = uicontrol('style','slider','string','wing opening'); 
+    ff.sl.w = uicontrol('style','slider','string','wing opening', 'tag','tag_sl_w'); 
     ff.sl.w.Max = max(w_steps);
     ff.sl.w.Min = min(w_steps);
     addlistener(ff.sl.w,'ContinuousValueChange',@(hObject, event) update_fig(hObject,event,w_steps,ff.sh.w,wing,pe,robot)); 
     
-    ff.sl.p = uicontrol('style','slider','string','body pitch'); 
+    ff.sl.p = uicontrol('style','slider','string','body pitch', 'tag','tag_sl_p');
     ff.sl.p.Max = max(p_steps);
     ff.sl.p.Min = min(p_steps);
     addlistener(ff.sl.p,'ContinuousValueChange',@(hObject, event) update_fig(hObject,event,p_steps,ff.sh.p,pitch,pe,robot)); 
     
-    ff.sl.r = uicontrol('style','slider','string','body roll'); 
+    ff.sl.r = uicontrol('style','slider','string','body roll', 'tag','tag_sl_r'); 
     ff.sl.r.Max = max(r_steps);
     ff.sl.r.Min = min(r_steps);
     addlistener(ff.sl.r,'ContinuousValueChange',@(hObject, event) update_fig(hObject,event,r_steps,ff.sh.r,roll,pe,robot)); 
@@ -97,7 +101,7 @@ function plot_righting
     addlistener(ff.bt,'ButtonDown',@(hObject, event) reset_fig(n0, wing(1,1,1), pitch(1,1,n0), roll(n0,1,1), pe, robot))
     
     for ii=1:3
-        ff.st(ii) = uicontrol('style','text','units','normalized','string','dof')
+        ff.st(ii) = uicontrol('style','text','units','normalized','tag',['var_' num2str(ii)]);
     end
     
     % configure ui components     
@@ -125,6 +129,8 @@ function update_fig(hObject, event, steps,sh,depvar,pe,robot)
             hh = findobj('tag','w_sec');
             gg = findobj('tag','sp_w_sec');  
             gg.Title.String=['wing opening = ' str_app num2str(abs(steps(min_idx))) '°'];
+            gg = findobj('tag','var_1');  
+            gg.String=['wing opening = ' str_app num2str(abs(steps(min_idx))) '°'];
             ss = findobj('tag','state_3d');
             ss.ZData = steps(min_idx);
             
@@ -134,6 +140,8 @@ function update_fig(hObject, event, steps,sh,depvar,pe,robot)
             hh = findobj('tag','p_sec');
             gg = findobj('tag','sp_p_sec'); 
             gg.Title.String=['body pitch = ' str_app num2str(abs(steps(min_idx))) '°'];
+            gg = findobj('tag','var_2');  
+            gg.String=['body pitch = ' str_app num2str(abs(steps(min_idx))) '°'];
             ss = findobj('tag','state_3d');
             ss.YData = steps(min_idx);
              
@@ -143,6 +151,8 @@ function update_fig(hObject, event, steps,sh,depvar,pe,robot)
             hh = findobj('tag','r_sec');
             gg = findobj('tag','sp_r_sec'); 
             gg.Title.String=['body roll = ' str_app num2str(abs(steps(min_idx))) '°'];
+            gg = findobj('tag','var_3');  
+            gg.String=['body roll = ' str_app num2str(abs(steps(min_idx))) '°'];
             ss = findobj('tag','state_3d');
             ss.XData = steps(min_idx);
 
@@ -175,25 +185,28 @@ function reset_fig(n0,w0,p0,r0,pe,robot)
     gg = findobj('tag','w_sec');    gg.ZData = squeeze(pe( :, 1, :));
     gg = findobj('tag','p_sec');    gg.ZData = squeeze(pe( :, :,n0));
     gg = findobj('tag','r_sec');    gg.ZData = squeeze(pe( n0, :, :));
+    % slider position
+    gg = findobj('tag','tag_sl_w');  gg.Value = w0;
+    gg = findobj('tag','tag_sl_r');  gg.Value = r0;
+    gg = findobj('tag','tag_sl_p');  gg.Value = p0;
+    % title string     
+    if(w0<0) str_app='−' ; else str_app=''; end
+    gg = findobj('tag','sp_w_sec'); gg.Title.String=['wing opening = '  str_app num2str(abs(w0)) '°'];
+    gg = findobj('tag','var_1');     gg.String=['wing opening = '  str_app num2str(abs(w0)) '°']; 
     
-    % reset title
-    gg = findobj('tag','sp_w_sec');  
-    if(w0<0) str_app='−' , else str_app=''; end
-    gg.Title.String=['wing opening = '  str_app num2str(abs(w0)) '°'];
-    
-    gg = findobj('tag','sp_p_sec');    
-    if(p0<0) str_app='−' , else str_app=''; end
-    gg.Title.String=['wing opening = '  str_app num2str(abs(p0)) '°'];
-    
-    gg = findobj('tag','sp_r_sec');  
-    if(r0<0) str_app='−' , else str_app=''; end
-    gg.Title.String=['wing opening = '  str_app num2str(abs(r0)) '°'];
+    if(p0<0) str_app='−' ; else str_app=''; end
+    gg = findobj('tag','sp_p_sec'); gg.Title.String=['body pitch = '  str_app num2str(abs(p0)) '°'];
+    gg = findobj('tag','var_2');     gg.String=['body pitch = '  str_app num2str(abs(p0)) '°'];
+          
+    if(r0<0) str_app='−' ; else str_app=''; end
+    gg = findobj('tag','sp_r_sec'); gg.Title.String=['body roll = '  str_app num2str(abs(r0)) '°'];
+    gg = findobj('tag','var_3');     gg.String=['body roll = '  str_app num2str(abs(r0)) '°'];
     
     % state markers
     ss = findobj('tag','state_3d');    ss.ZData = w0;    ss.YData = p0;    ss.XData = r0;
-    ss = findobj('tag','state_cw');    ss.XData = r0;    ss.YData = p0;    
-    ss = findobj('tag','state_cr');    ss.XData = p0;    ss.YData = w0;   
-    ss = findobj('tag','state_cp');    ss.XData = r0;    ss.YData = w0;   
+    ss = findobj('tag','state_cw');    ss.XData = r0;    ss.YData = p0;    ss.ZData = 2000;
+    ss = findobj('tag','state_cr');    ss.XData = p0;    ss.YData = w0;    ss.ZData = 2000;
+    ss = findobj('tag','state_cp');    ss.XData = r0;    ss.YData = w0;    ss.ZData = 2000;
      
     % robot
     wing_ip  = w0;
